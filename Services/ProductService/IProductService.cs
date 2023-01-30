4dbba7ae4bade54a76e7615921cd95518a9e992d
itaivincent <itaivincent321@gmail.com>
2023-01-30 14:55:18 +0200

    async on the service

--- Services/ProductService/IProductService.cs
+++ Services/ProductService/IProductService.cs
@@ -4,7 +4,7 @@ namespace BawlAPI.Services.ProductService
 {
     public interface IProductService
     {
-        List<Product> Get();
+        Task<List<Product>> Get();
         List<Product> AddProduct(Product product);
         List<Product> UpdateProduct(Product request);
         Product GetProduct(int id);
