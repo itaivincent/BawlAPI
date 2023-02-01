e76ab674bf036b0f21689a0853fd7da373a0f73a
itaivincent <itaivincent321@gmail.com>
2023-02-01 02:00:53 +0200

    dtos

--- Dtos/Product/GetProductDto.cs
+++ Dtos/Product/GetProductDto.cs
@@ -2,5 +2,14 @@
 {
     public class GetProductDto
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int CategoryId { get; set; }
+        public int InventoryId { get; set; }
+        public float Price { get; set; }
+        public int DiscountId { get; set; }
+        public string? CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
     }
 }
