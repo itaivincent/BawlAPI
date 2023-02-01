7325c3575b2ea521dcf380550dd4773cf24e5d29
itaivincent <itaivincent321@gmail.com>
2023-02-01 02:35:02 +0200

    register automapper

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<DataContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//registering automapper
+
 //letting the API know that it has to use the ProductService class whenever a controller wants to inject the IProductService
 builder.Services.AddScoped<IProductService, ProductService>();
 
