6a084d20fa65f8ed66a6c30bc5e77e13c7c2ae29
itaivincent <itaivincent321@gmail.com>
2023-02-01 02:12:38 +0200

    implement dto in service

--- Services/ProductService/IProductService.cs
+++ Services/ProductService/IProductService.cs
@@ -1,12 +1,13 @@
-﻿using BawlAPI.Models;
+﻿using BawlAPI.Dtos.Product;
+using BawlAPI.Models;
 
 namespace BawlAPI.Services.ProductService
 {
     public interface IProductService
     {
-        Task<ServiceResponse<List<Product>>> Get();
-        Task<ServiceResponse<List<Product>>> AddProduct(Product product);
-        Task<ServiceResponse<List<Product>>> UpdateProduct(Product request);
-        Task<ServiceResponse<Product>>  GetProduct(int id);
+        Task<ServiceResponse<List<GetProductDto>>> Get();
+        Task<ServiceResponse<List<GetProductDto>>> AddProduct(Product product);
+        Task<ServiceResponse<List<GetProductDto>>> UpdateProduct(Product request);
+        Task<ServiceResponse<GetProductDto>>  GetProduct(int id);
     }
 }
