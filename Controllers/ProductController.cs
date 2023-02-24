5c6680e92db4124b527419a1c5647fffaa8f01d1
itaivincent <itaivincent321@gmail.com>
2023-02-24 11:53:34 +0200

    tests

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -48,7 +48,6 @@ namespace BawlAPI.Controllers
         public async Task<ActionResult<ServiceResponse<GetProductDto>>> GetProduct(int id)
         {
             return Ok( await _productService.GetProduct(id));
-
         }
 
     }
