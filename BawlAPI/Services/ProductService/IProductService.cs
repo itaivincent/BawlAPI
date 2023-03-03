fec39eb48d0120c42c5b490abbefdd1c9ee006d8
itaivincent <itaivincent321@gmail.com>
2023-03-03 22:31:27 +0200

    deployments

--- BawlAPI/Services/ProductService/IProductService.cs
+++ BawlAPI/Services/ProductService/IProductService.cs
@@ -6,6 +6,7 @@ namespace BawlAPI.Services.ProductService
     public interface IProductService
     {
         List<Product> Get();
+
         Task<ServiceResponse<List<GetProductDto>>> AddProduct(AddProductDto product);
         Task<ServiceResponse<List<GetProductDto>>> UpdateProduct(UpdateProductDto request);
         Task<ServiceResponse<GetProductDto>>  GetProduct(int id);
